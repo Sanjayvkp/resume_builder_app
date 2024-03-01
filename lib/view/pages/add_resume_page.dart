ca67550d6b6e3e77cb36ed5e94560da30082b5e7
Sanjayvkp <gsanjayvkp000@gmail.com>
2024-03-01 11:32:30 +0530

    added card widget
    added card widget to add title,content

--- lib/view/pages/add_resume_page.dart
+++ lib/view/pages/add_resume_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resume_builder_app/view/widgets/text_field_widget.dart';
 
 class AddResumePage extends StatelessWidget {
   const AddResumePage({super.key});
@@ -7,10 +8,20 @@ class AddResumePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Resume'),
+        title: const Text('Resume'),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.remove_red_eye))
+          IconButton(onPressed: () {}, icon: const Icon(Icons.remove_red_eye))
         ],
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: CardWidget(height: 60),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/view/widgets/text_field_widget.dart
+++ lib/view/widgets/text_field_widget.dart
@@ -1,23 +1,60 @@
 import 'package:flutter/material.dart';
 
-class TextFieldWidget extends StatelessWidget {
+class CardWidget extends StatelessWidget {
   final double height;
-  const TextFieldWidget({super.key, required this.height});
+  const CardWidget({super.key, required this.height});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: height,
-      width: 350,
-      decoration:
-          BoxDecoration(border: Border.all(width: 1, color: Colors.black)),
-      child: const TextField(
-        decoration: InputDecoration(
-            border: InputBorder.none,
-            labelText: 'Title',
-            hintText: 'Enter title here...'),
+      color: Colors.grey.withOpacity(.20),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(
+          children: [
+            Container(
+              height: height,
+              width: MediaQuery.sizeOf(context).width,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.black)),
+              child: const TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Title',
+                    hintText: 'Enter title here...'),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 150,
+              width: MediaQuery.sizeOf(context).width,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.black)),
+              child: const TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter content here...'),
+              ),
+            ),
+            ListTile(
+              leading: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.arrow_upward)),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_downward_outlined)),
+                ],
+              ),
+              trailing:
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+            )
+          ],
+        ),
       ),
     );
-    ;
   }
 }
